34252e18261b9655599d5183fbc91ab202683eba
Charcoalie06 <charliekock06@gmail.com>
2025-10-14 15:46:49 +0200

    sigma

--- lab3/graphicsmain.py
+++ lab3/graphicsmain.py
@@ -1,14 +1,6 @@
 from gamemodel import *
 from graphics import *
 
-#Dummy Values until i get the REAL values from gamemodel.
-player0pos = -110
-player1pos = 110
-player0col = "Blue"
-player1col =  "Red"
-player0size = 3
-player1size = 3
-
 class GameGraphics:
     def __init__(self, game):
         self.game = game
@@ -27,44 +19,25 @@ class GameGraphics:
 
     def drawCanon(self,playerNr):
         # draw the cannon
-        '''IF SOMETHING DOESNT WORK HERE, PLEASE LET ME KNOW AND I WILL FIX IT!
-        BUT I WROTE THIS CODE WHEN NONE OF THE FUCTIONS I NEEDED WAS CREATED SO THERE IS A STRONG
-        POSSIBILITY THAT A LARGE AMOUNT OF MY CODE IS GARBAGE PLS LET ME KNOW BYEBYE! - Charlie'''
 
         #Calls player number.
         player = self.game.getPlayers()[playerNr]
 
         #Creates both cubes, gives them a position, gives them a color.
-        cannon0 = Rectangle(Point(player0pos,0),Point(player0pos+player0size,player0size))
-        cannon1 = Rectangle(Point(player1pos,0),Point(player1pos+player1size,player1size))
-        cannon0.setFill(player0col)
-        cannon1.setFill(player1col)
-
-        #If player 0 wants to be used, then they draw it, and same with player 1.
-        if playerNr == 0:
-            cannon0.draw(self.win)
-            return cannon0
-        if playerNr == 1:
-            cannon1.draw(self.win)
-            return cannon1
-        return None
+        cannon = Rectangle(Point(player.position,0),Point(player.position+self.game.cannonSize,self.game.cannonSize))
+        cannon.setFill(player.color)
+        cannon.draw(self.win)
+        return cannon
 
     def drawScore(self,playerNr):
         # draw the score
 
+        #Calls player number.
+        player = self.game.getPlayers()[playerNr]
+
         #Draws two separate texts under each player.
         #The "-playerXpos/2" part means that i position the text below the cannon, and the distance down is half the size of the cannon.
-        text0 = Text(Point(player0pos,-player0pos/2),"Score 0") 
-        text1 = Text(Point(player1pos,-player0pos/2),"Score 0")
-
-        #If text 0 wants to be used, then they draw it, and same with text 1.
-        if playerNr == 0:
-            text0.draw(self.win)
-            return text0
-        if playerNr == 1:
-            text1.draw(self.win)
-            return text1
-        return None
+        text = Text(Point(player.position,player.position/-2),"Score 0") 
 
     def fire(self, angle, vel):
         player = self.game.getCurrentPlayer()
